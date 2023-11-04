698858f1adedb556f4f3c7fee9114c87eb60f54e
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2023-11-05 00:03:32 +0300

    fix:  change handlePhrases phrases component

--- src/components/phrases.jsx
+++ src/components/phrases.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 
 const Phrases = ({ users }) => {
-    // console.log(users);
-
     const getClasses = () => {
         let classes = "badge m-2 bg-";
         classes += users.length === 0 ? "danger" : "primary";
         return classes;
     };
     const handlePhrases = (users) => {
+        const numberAsString = users.length.toString();
+        const lastTwoDigits = parseInt(numberAsString.slice(-2));
+
         if (users.length === 0) return "Никто с тобой сегодня не тусанет!";
-        if ([2, 3, 4].includes(users.length))
+        if (
+            [2, 3, 4].includes(lastTwoDigits % 10) &&
+            ![12, 13, 14].includes(lastTwoDigits)
+        ) {
             return `${users.length} человека тусанут с тобой сегодня`;
+        }
         return `${users.length} человек тусанёт с тобой сегодня`;
+
+        /*return users.length === 0
+            ? "Никто с тобой сегодня не тусанет!"
+            : [2, 3, 4].includes(lastTwoDigits % 10) &&
+              ![12, 13, 14].includes(lastTwoDigits)
+            ? `${users.length} человека тусанут с тобой сегодня`
+            : `${users.length} человек тусанёт с тобой сегодня`;*/
     };
     return (
         <h2>
