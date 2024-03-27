812d6acfcb0a3d65ba6656b2c0bc302812112f51
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-03-27 23:57:19 +0300

    fix: change Users component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -5,13 +5,14 @@ import Pagination from "./Pagination";
 import { paginate } from "../utils/paginate";
 import GroupList from "./GroupList";
 import _ from "lodash";
+import SearchStatus from "./SearchStatus";
 
 const Users = ({ users, professions, ...rest }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [selectedProf, setSelectedProf] = useState({});
     console.log("users: ", users);
 
-    const PAGE_SIZE = 2;
+    const PAGE_SIZE = 4;
     const handleChangePage = (page) => {
         setCurrentPage(page);
     };
@@ -41,55 +42,64 @@ const Users = ({ users, professions, ...rest }) => {
     console.log("count: ", count);
 
     return (
-        <>
-            {professions && (
-                <>
-                    <GroupList
-                        items={professions}
-                        selectedItems={selectedProf}
-                        onItemSelect={handleProfessionSelect}
-                    />
-                    <button
-                        className="btn btn-secondary mx-2"
-                        onClick={handleClearFilter}
-                    >
-                        Clear
-                    </button>
-                </>
-            )}
-
-            {count > 0 && (
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Имя</th>
-                            <th scope="col">Качества</th>
-                            <th scope="col">Провфессия</th>
-                            <th scope="col">Встретился, раз</th>
-                            <th scope="col">Оценка</th>
-                            <th scope="col">Избранное</th>
-                            <th />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {usersSlice.map((user, i) => (
-                            <User
-                                key={user._id}
-                                index={i}
-                                {...user}
-                                {...rest}
+        <div className="d-flex flex-column">
+            <div className="d-flex justify-content-center">
+                <SearchStatus length={count} />
+            </div>
+            <div className="col-10 justify-content-center">
+                <div className="d-flex">
+                    {professions && (
+                        <div className="d-flex flex-column">
+                            <GroupList
+                                items={professions}
+                                selectedItems={selectedProf}
+                                onItemSelect={handleProfessionSelect}
                             />
-                        ))}
-                    </tbody>
-                </table>
-            )}
-            <Pagination
-                pageCount={pageCount}
-                currentPage={currentPage}
-                onPageChange={handleChangePage}
-            />
-        </>
+                            <button
+                                className="btn btn-secondary mx-2"
+                                onClick={handleClearFilter}
+                            >
+                                Clear
+                            </button>
+                        </div>
+                    )}
+
+                    {count > 0 && (
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Имя</th>
+                                    <th scope="col">Качества</th>
+                                    <th scope="col">Профессия</th>
+                                    <th scope="col">Встретился, раз</th>
+                                    <th scope="col">Оценка</th>
+                                    <th scope="col">Избранное</th>
+                                    <th />
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {usersSlice.map((user, i) => (
+                                    <User
+                                        key={user._id}
+                                        index={i}
+                                        {...user}
+                                        {...rest}
+                                    />
+                                ))}
+                            </tbody>
+                        </table>
+                    )}
+                </div>
+            </div>
+            <div className="d-flex justify-content-center mt-3">
+                <Pagination
+                    pageCount={pageCount}
+                    currentPage={currentPage}
+                    onPageChange={handleChangePage}
+                />
+            </div>
+        </div>
     );
 };
 
