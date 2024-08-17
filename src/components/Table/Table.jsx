8be54e00717c2c4859094caa524953980dbeab7b
js-neo <yuriy.jsx@gmail.com>
2024-08-17 23:51:57 +0300

    fix: change TableHead component

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,31 +5,38 @@ import TableHead from "./TableHead";
 
 const Table = ({ users, startIndex, ...rest }) => {
     const columns = {
-        number: { name: "#" },
+        number: { _id: 1, name: "#" },
         name: {
+            _id: 2,
             iter: "name",
             name: "Name"
         },
         qualities: {
+            _id: 3,
             name: "Qualities"
         },
         profession: {
+            _id: 4,
             iter: "profession.name",
             name: "Profession"
         },
         completedMeetings: {
+            _id: 5,
             iter: "completedMeetings",
             name: "CompletedMeetings"
         },
         rate: {
+            _id: 6,
             iter: "rate",
             name: "Rate"
         },
         favorites: {
+            _id: 7,
             iter: "status",
             name: "Favorites"
         },
         delete: {
+            _id: 8,
             name: "Delete"
         }
     };
@@ -40,7 +47,12 @@ const Table = ({ users, startIndex, ...rest }) => {
             <tbody>
                 {users.map((user, i) => {
                     return (
-                        <User key={user._id} index={i + startIndex} {...user} {...rest} />
+                        <User
+                            key={user._id}
+                            index={i + startIndex}
+                            {...user}
+                            {...rest}
+                        />
                     );
                 })}
             </tbody>
--- src/components/Table/TableHead.jsx
+++ src/components/Table/TableHead.jsx
@@ -1,32 +1,39 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 
 const TableHead = ({ onSort, columns, selectedSort }) => {
-    const handleSort = (iter) => {
-        iter === selectedSort.iter
-            ? onSort({
-                  ...selectedSort,
-                  order: selectedSort.order === "asc" ? "desc" : "asc"
-              })
-            : onSort({
-                  iter,
-                  order: "asc"
-              });
-    };
+    const handleSort = useCallback(
+        (iter) => {
+            iter === selectedSort.iter
+                ? onSort({
+                      ...selectedSort,
+                      order: selectedSort.order === "asc" ? "desc" : "asc"
+                  })
+                : onSort({
+                      iter,
+                      order: "asc"
+                  });
+        },
+        [onSort, selectedSort]
+    );
+
     return (
         <thead>
             <tr>
-                {Object.keys(columns).map((column) => (
-                    <th
-                        key={columns[column]._id}
-                        {...(columns[column].iter && {
-                            onClick: () => handleSort(columns[column].iter),
-                            role: "button"
-                        })}
-                    >
-                        {columns[column].name}
-                    </th>
-                ))}
+                {Object.keys(columns).map((column) => {
+                    const { _id, iter, name } = columns[column];
+                    return (
+                        <th
+                            key={_id}
+                            {...(iter && {
+                                onClick: () => handleSort(iter),
+                                role: "button"
+                            })}
+                        >
+                            {name}
+                        </th>
+                    );
+                })}
             </tr>
         </thead>
     );
