377e651bd0e6b516d0eb5ff1f7462e72443a964c
js-neo <yuriy.jsx@gmail.com>
2024-09-02 23:59:15 +0300

    feat: add form to Login component

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleChange = (type, value) => {
+        const setters = {
+            email: setEmail,
+            password: setPassword
+        };
+
+        const setter = setters[type];
+        if (setter) {
+            setter(value);
+        }
+    };
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={(e) => handleChange("email", e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => handleChange("password", e.target.value)}
+                />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
