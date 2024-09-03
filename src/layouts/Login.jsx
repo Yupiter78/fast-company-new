6ffd5270a490824f2ab1e37241038fb6d726d4d6
js-neo <yuriy.jsx@gmail.com>
2024-09-04 00:06:22 +0300

    feat: add validate method to Login component

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -1,13 +1,38 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import TextField from "../components/TextField";
 
 const Login = () => {
     const [formData, setFormData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
     const handleChange = useCallback(({ target: { name, value } }) => {
         setFormData((prevState) => ({ ...prevState, [name]: value }));
     }, []);
+
+    useEffect(() => {
+        validate();
+    }, [formData]);
+    const validate = () => {
+        const errors = {};
+        for (const key in formData) {
+            if (Object.prototype.hasOwnProperty.call(formData, key)) {
+                if (formData[key].trim() === "") {
+                    errors[key] = `Field ${key} is required`;
+                }
+            }
+        }
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isValid = validate();
+        console.log("errors: ", errors);
+        if (!isValid) return;
+        console.log("formData: ", formData);
+    };
+
     return (
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <TextField
                 label="Email"
                 name="email"
@@ -21,6 +46,7 @@ const Login = () => {
                 value={formData.password}
                 onChange={handleChange}
             />
+            <button type="submit">Login</button>
         </form>
     );
 };
