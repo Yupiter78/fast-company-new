be53ba5c0bad0100f79d6beaf3c90feb057971c5
js-neo <yuriy.jsx@gmail.com>
2024-09-19 01:06:43 +0300

    fix: change validator.js

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,17 +1,16 @@
 export function validator(data, config) {
     const errors = {};
-    const validate = (method, propData, message) => {
-        switch (method) {
-            case "isRequired":
-                if (propData.trim() === "") return message;
-                break;
+    const validate = {
+        isRequired: (propData, message) => {
+            if (propData.trim() === "") return message;
         }
     };
+
     for (const [fieldName, content] of Object.entries(data)) {
         for (const [validateMethod, { message }] of Object.entries(
             config[fieldName]
         )) {
-            const errorMessage = validate(validateMethod, content, message);
+            const errorMessage = validate[validateMethod]?.(content, message);
             if (errorMessage) errors[fieldName] = errorMessage;
         }
     }
