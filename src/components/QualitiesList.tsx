3833d5a51c282e780e6703564b049671bd69f929
js-neo <yuriy.jsx@gmail.com>
2024-09-26 23:43:38 +0300

    refactor: change QualitiesList.[jsx=>tsx]

---
+++ src/components/QualitiesList.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+import Quality from "./Quality";
+import { Quality as QualityProps } from "../types/types";
+
+interface QualitiesListProps {
+    qualities: QualityProps[];
+}
+
+const QualitiesList: FC<QualitiesListProps> = ({ qualities }) => {
+    return (
+        <>
+            {qualities.map((quality) => (
+                <Quality key={quality._id} {...quality} />
+            ))}
+        </>
+    );
+};
+
+export default QualitiesList;
--- src/components/Quality.tsx
+++ src/components/Quality.tsx
@@ -1,9 +1,5 @@
 import React, { FC } from "react";
-
-interface QualityProps {
-    color: string;
-    name: string;
-}
+import { Quality as QualityProps } from "../types/types";
 
 const Quality: FC<QualityProps> = ({ color, name }) => {
     return <span className={`badge mx-2 bg-${color}`}>{name}</span>;
