2f0966eb51d22e5c4925ff7b6043ec3c712da09f
js-neo <yuriy.jsx@gmail.com>
2024-10-01 01:31:25 +0300

    fix: change reportWebVitals.ts

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,12 +1,16 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB, Metric } from "web-vitals";
+import { Metric } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry?: (metric: Metric) => void) => {
     if (onPerfEntry instanceof Function) {
-        getCLS(onPerfEntry);
-        getFID(onPerfEntry);
-        getFCP(onPerfEntry);
-        getLCP(onPerfEntry);
-        getTTFB(onPerfEntry);
+        import("web-vitals").then(
+            ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+                getCLS(onPerfEntry);
+                getFID(onPerfEntry);
+                getFCP(onPerfEntry);
+                getLCP(onPerfEntry);
+                getTTFB(onPerfEntry);
+            }
+        );
     }
 };
 
